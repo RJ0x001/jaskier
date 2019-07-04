25f8667f2500b7724ad55fae85b569e0ee85301e
rj0x001 <atrem.e.pavlenko@gmail.com>
2019-07-04 21:36:29 +0300

    new lyrics base and visual

--- app/views.py
+++ app/views.py
@@ -1,30 +1,25 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from app import app
 from functions import get_lyrics
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'RedJin'}
-    artist, song, lyrics = get_lyrics(user['nickname'])
-    if not lyrics:
-        return render_template('error.html', artist=artist, song=song)
+@app.route('/<username>/')
+def lyrics_page(username):
+    d = get_lyrics(username)
+    if not d['page']:
+        return render_template('error.html', error_msg='Username %s does not exist on last.fm' % username)
+    if not d['lyrics']:
+        return render_template('error.html', error_msg='No lyrics found for %s - %s' % (d['artist'], d['song']))
     return render_template('index.html',
-                           artist=artist,
-                           song=song,
-                           lyrics=lyrics.split('\n'))
+                           artist=d['artist'],
+                           song=d['song'],
+                           lyrics=d['lyrics'].split('\n'))
 
 
-@app.route('/auth', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/start/', methods=['GET', 'POST'])
 def auth():
     if request.method == 'POST':
         username = str(request.form.get('username'))
-        artist, song, lyrics, page_check = get_lyrics(username)
-        if not lyrics:
-            return render_template('error.html', artist=artist, song=song)
-        return render_template('index.html',
-                               artist=artist,
-                               song=song,
-                               lyrics=lyrics.split('\n'))
-    return render_template('auth.html')
+        return redirect(url_for('lyrics_page', username=username))
+    return render_template('start.html')
--- functions.py
+++ functions.py
@@ -1,24 +1,24 @@
-from PyLyrics import *
+# from PyLyrics import *
 from bs4 import BeautifulSoup
+import lyricwikia
+import requests
 
 
 def get_lyrics(user):
     last = requests.get('https://www.last.fm/user/%s' % user)
-    soup = BeautifulSoup(last.content, "lxml")
+    soup = BeautifulSoup(last.content, "html.parser")
+    d = dict()
     try:
-        chart = soup.find('td', 'chartlist-name').findAll('a')
+        d['song'] = soup.find('td', 'chartlist-name').findAll('a')[0].get('title')
+        d['artist'] = soup.find('td', 'chartlist-artist').findAll('a')[0].get('title')
+        d['page'] = 1
     except AttributeError:
-        pass
-    c, content = 0, ''
-    for x in chart:
-        if c == 1:
-            content = x.get('title')
-        x.get('title')
-        c += 1
-    artist, song = content.strip().split('—')
+        d['page'] = 0
+        return d
     try:
-        lyrics = PyLyrics.getLyrics(artist.strip(), song.strip())
-    except ValueError:
-        lyrics = None
-    return artist, song, lyrics
+        d['lyrics'] = lyricwikia.get_lyrics(d['artist'].strip(), d['song'].strip())
+    except Exception as error:
+        print(error)
+        d['lyrics'] = None
+    return d
 
